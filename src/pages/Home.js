0417dd8725a7b7cb225e3e3bed6d18a5d583a04e
naeem <kazinaeemrayhan@gmail.com>
2022-05-23 01:50:44 +0600

    BusinessSummary added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../components/Home/Banner";
+import BusinessSummary from "../components/Home/BusinessSummary";
 import useProducts from "../hooks/useProducts";
 
 const Home = () => {
@@ -9,7 +10,7 @@ const Home = () => {
       <Banner></Banner>
       <div className="container mx-auto my-14">
         <h2 className="text-5xl text-center mb-14">Tools</h2>
-        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-5 mx-5 sm:mx-0 lg:mx-8">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center gap-5 mx-5 sm:mx-0 lg:mx-8">
           {products.slice(0, 6).map(product =>
           (<div class="card card-compact  w-full bg-base-100 shadow-xl">
             <figure>
@@ -28,6 +29,7 @@ const Home = () => {
           </div>))}
         </div>
       </div>
+      <BusinessSummary />
     </div>
   );
 };
